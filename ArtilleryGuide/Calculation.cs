ffc743d49ddbaeb1c78527f84774d5d025142f8d
Erik Schulze <erikmail3333@gmail.com>
2021-03-09 16:40:27 +0100

    Projektdateien hinzufügen.

---
+++ ArtilleryGuide/Calculation.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtilleryGuide
+{
+    public static class Util
+    {
+        public static double RadiansToDegrees(double radians)
+        {
+            double degrees = (180 / Math.PI) * radians;
+            return (degrees);
+        }
+
+        public static double DegreesToRadians(double angle)
+        {
+            return (Math.PI / 180) * angle;
+        }
+
+        public static double NormalizeHeading(double heading)
+        {
+            double ranged_heading = heading % 360.0;
+            if (ranged_heading < 0.0)
+            {
+                return ranged_heading + 360.0;
+            }
+            else
+            {
+                return ranged_heading;
+            }
+        }
+    }
+
+
+    public struct Vector2D
+    {
+        public double x;
+        public double y;
+
+        public double Length()
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+
+        public void Normalize()
+        {
+            double length = Length();
+
+            x /= length;
+            y /= length;
+        }
+
+        public Vector2D Normalized()
+        {
+            double length = Length();
+
+            return new Vector2D
+            {
+                x = x / length,
+                y = y / length
+            };
+        }
+
+        public double Heading()
+        {
+            double heading_radians = Math.Atan2(y, x);
+            double heading_reverse_from_x = Util.RadiansToDegrees(heading_radians);
+            double heading_from_x = Util.NormalizeHeading(-heading_reverse_from_x);
+            double heading_from_y = Util.NormalizeHeading(heading_from_x + 90.0);
+            return heading_from_y;
+        }
+
+        public static Vector2D FromHeading(double Heading)
+        {
+            double heading_from_x = Util.NormalizeHeading(Heading - 90);
+            double reverse_heading_from_x = -heading_from_x;
+            return new Vector2D
+            {
+                x = Math.Cos(Util.DegreesToRadians(reverse_heading_from_x)),
+                y = Math.Sin(Util.DegreesToRadians(reverse_heading_from_x)),
+            };
+        }
+
+        public static Vector2D operator + (Vector2D a, Vector2D b)
+        {
+            return new Vector2D
+            {
+                x = a.x + b.x,
+                y = a.y + b.y
+            };
+        }
+
+        public static Vector2D operator - (Vector2D a, Vector2D b)
+        {
+            return new Vector2D
+            {
+                x = a.x - b.x,
+                y = a.y - b.y
+            };
+        }
+
+        public static Vector2D operator - (Vector2D a)
+        {
+            return new Vector2D
+            {
+                x = -a.x,
+                y = -a.y
+            };
+        }
+
+        public static Vector2D operator * (Vector2D a, double b)
+        {
+            return new Vector2D
+            {
+                x = a.x * b,
+                y = a.y * b
+            };
+        }
+
+        public static Vector2D FromHeadingInformation(HeadingInformation info)
+        {
+            Vector2D unit_vector = FromHeading(info.heading);
+            return unit_vector * info.distance;
+        }
+    }
+
+    public struct HeadingInformation
+    {
+        public double heading;
+        public double distance;
+
+        public static HeadingInformation operator - (HeadingInformation a)
+        {
+            return new HeadingInformation
+            {
+                heading = Util.NormalizeHeading(a.heading - 180.0),
+                distance = a.distance
+            };
+        }
+
+        public static HeadingInformation operator + (HeadingInformation a, HeadingInformation b)
+        {
+            Vector2D a_vec = Vector2D.FromHeadingInformation(a);
+            Vector2D b_vec = Vector2D.FromHeadingInformation(b);
+
+            Vector2D total_vec = a_vec + b_vec;
+
+            double distance = total_vec.Length();
+            double heading = total_vec.Normalized().Heading();
+
+            return new HeadingInformation
+            {
+                distance = distance,
+                heading = heading
+            };
+        }
+    }
+}
---
+++ ArtilleryGuide/MainWindow.xaml.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArtilleryGuide
+{
+
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private double _heading = 0.0;
+        public double Heading
+        {
+            get
+            {
+                return _heading;
+            }
+            set
+            {
+                _heading = value;
+                Recalculate();
+            }
+        }
+
+        private double _distance = 100.0;
+        public double Distance
+        {
+            get
+            {
+                return _distance;
+            }
+            set
+            {
+                _distance = value;
+                Recalculate();
+            }
+        }
+        ObservableCollection<RangeInformation> hops;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            hops = new ObservableCollection<RangeInformation>();
+
+            hops.Add(new RangeInformation() { RangeName = "Last Hop to Gun", Azimuth = 180.0, Distance = 100.0 });
+            hops[0].PropertyChanged += ListItem_PropertyChanged;
+            HopsList.ItemsSource = hops;
+            UpdateRemoveHopButton();
+
+            hops.CollectionChanged += Hops_CollectionChanged;
+            Recalculate();
+        }
+
+        private void EnemyHeading_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            Recalculate();
+        }
+
+        private void ListItem_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            Recalculate();
+        }
+
+        private void Hops_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            Recalculate();
+        }
+
+        private void AddHop_Click(object sender, RoutedEventArgs e)
+        {
+            hops.Insert(0, new RangeInformation() { RangeName = "First Hop from Spotter", Azimuth = 180.0, Distance = 100.0 });
+            hops[0].PropertyChanged += ListItem_PropertyChanged;
+            UpdateHopNames();
+
+            UpdateRemoveHopButton();
+        }
+
+        private void RemoveHop_Click(object sender, RoutedEventArgs e)
+        {
+            hops.RemoveAt(0);
+
+            UpdateHopNames();
+
+            UpdateRemoveHopButton();
+        }
+
+        private void UpdateRemoveHopButton()
+        {
+            RemoveHopButton.IsEnabled = hops.Count > 1;
+        }
+
+        private void UpdateHopNames()
+        {
+            if (hops.Count == 1)
+            {
+                hops[0].RangeName = "Spotter to Gun";
+            }
+            else {
+                hops[0].RangeName = "First Hop from Spotter";
+                hops[hops.Count - 1].RangeName = "Last Hop to Gun";
+            }
+
+            for(int i = 1; i < hops.Count - 1; i++)
+            {
+                hops[i].RangeName = string.Format("Hop {0}", i);
+            }
+        }
+
+        private void Recalculate()
+        {
+            HeadingInformation enemy = new HeadingInformation
+            {
+                heading = _heading,
+                distance = _distance
+            };
+
+            List<HeadingInformation> gunHeadings = new List<HeadingInformation>();
+            foreach(RangeInformation rangeInformation in hops)
+            {
+                gunHeadings.Add(new HeadingInformation()
+                {
+                    heading = rangeInformation.Azimuth,
+                    distance = rangeInformation.Distance
+                });
+            }
+
+            HeadingInformation totalGunHeading = new HeadingInformation()
+            {
+                heading = 0.0,
+                distance = 0.0
+            };
+
+            foreach(HeadingInformation headingInformation in gunHeadings)
+            {
+                totalGunHeading += headingInformation;
+            }
+
+            HeadingInformation reversedGunHeading = -totalGunHeading;
+            HeadingInformation taskingHeading = reversedGunHeading + enemy;
+
+            CalculatedAzimuthBox.Text = taskingHeading.heading.ToString();
+            CalculatedDistanceBox.Text = taskingHeading.distance.ToString();
+        }
+    }
+
+    public class RangeInformation : INotifyPropertyChanged
+    {
+        private string _rangeName;
+        public string  RangeName { 
+            get 
+            {
+                return _rangeName;
+            } 
+            set 
+            {
+                _rangeName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("RangeName"));
+            } 
+        }
+        private double _azimuth;
+        public double Azimuth { 
+            get 
+            {
+                return _azimuth;
+            } 
+            set 
+            {
+                _azimuth = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Azimuth"));
+            } 
+        }
+        private double _distance;
+        public double Distance { 
+            get 
+            {
+                return _distance;
+            } 
+            set 
+            {
+                _distance = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Distance"));
+            } 
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
